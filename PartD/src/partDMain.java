1ebe6cfa76a85e9f9ad6dc0e42dbfd461ffba052
Maniclavender <maniclavender@gmail.com>
2019-01-18 19:33:48 +1300

    fourthline

--- PartD/src/partDMain.java
+++ PartD/src/partDMain.java
@@ -7,7 +7,7 @@ public class partDMain {
 		System.out.println("Hi, my name is Mandy.");
 		System.out.println("My favourite colour is green.");
 		System.out.println("My favourite mode of transportation is walking.");
-		
+		System.out.println("The best day of the week is Saturday.");
 		
 	}
 
